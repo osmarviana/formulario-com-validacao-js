2be6e33354147401d560e63b612965ed721a96dd
Osmar Viana <osmarvianatorres@gmail.com>
2023-03-16 16:35:55 -0300

    corrigindo quest

--- src/js/script.js
+++ src/js/script.js
@@ -1,97 +1,39 @@
 const formulario = document.getElementById("formulario");
+const inputText = document.querySelectorAll(".input");
 
-const nomeDigitado = document.getElementById("nome");
-let validacaoNome = false;
+inputText.forEach((input) => {
+  formulario.addEventListener("submit", (event) => {
+    // event.preventDefault();
 
-const emailDigitado = document.getElementById("email");
-let validacaoEmail = false;
+    if (input.value === "") {
+      console.log("PREENCHA!");
 
-const telefoneDigitado = document.getElementById("telefone");
-let validacaoTelefone = false;
+      input.classList.remove("sucesso-preenchimento");
+      input.classList.add("erro-preenchimento");
 
-const mensagemDigitada = document.getElementById("mensagem");
-let validacaoMensagem = false;
+      setErrorFor(input, "Campo obrigatório!");
+    } else {
+      console.log("Muito bem!");
 
-const mensagemErro = document.querySelectorAll(".mensagem-erro");
+      input.classList.add("sucesso-preenchimento");
+      input.classList.remove("erro-preenchimento");
 
-formulario.addEventListener("submit", function (event) {
-  event.preventDefault();
-
-  validarInputs();
-  cadastrar();
+      setSuccessFor(input);
+    }
+  });
 });
 
-function validarInputs() {
-  if (nomeDigitado.value.length <= 2) {
-    nomeDigitado.classList.add("erro-preenchimento");
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
 
-    mensagemErro[0].classList.add("ativar-msg-erro");
-    validacaoNome = false;
-  } else {
-    nomeDigitado.classList.remove("erro-preenchimento");
-    nomeDigitado.classList.add("sucesso-preenchimento");
+  small.innerText = message;
 
-    mensagemErro[0].classList.remove("ativar-msg-erro");
-    validacaoNome = true;
-  }
-
-  if (emailDigitado.value === "" || !validacaoDeEmail(emailDigitado.value)) {
-    emailDigitado.classList.add("erro-preenchimento");
-
-    mensagemErro[1].classList.add("ativar-msg-erro");
-    validacaoEmail = false;
-  } else {
-    emailDigitado.classList.remove("erro-preenchimento");
-    emailDigitado.classList.add("sucesso-preenchimento");
-
-    mensagemErro[1].classList.remove("ativar-msg-erro");
-    validacaoEmail = true;
-  }
-
-  if (telefoneDigitado.value.length <= 9) {
-    telefoneDigitado.classList.add("erro-preenchimento");
-
-    mensagemErro[2].classList.add("ativar-msg-erro");
-    validacaoTelefone = false;
-  } else {
-    telefoneDigitado.classList.remove("erro-preenchimento");
-    telefoneDigitado.classList.add("sucesso-preenchimento");
-
-    mensagemErro[2].classList.remove("ativar-msg-erro");
-    validacaoTelefone = true;
-  }
-
-  if (mensagemDigitada.value === "") {
-    mensagemDigitada.classList.add("erro-preenchimento");
-
-    mensagemErro[3].classList.add("ativar-msg-erro");
-    validacaoMensagem = false;
-  } else {
-    mensagemDigitada.classList.remove("erro-preenchimento");
-    mensagemDigitada.classList.add("sucesso-preenchimento");
-
-    mensagemErro[3].classList.remove("ativar-msg-erro");
-    validacaoMensagem = true;
-  }
+  formControl.className = "form-control error";
 }
 
-function validacaoDeEmail(email) {
-  const emailRegex = new RegExp(
-    /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
-  );
-  if (emailRegex.test(email)) {
-    return true;
-  }
-  return false;
-}
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
 
-function cadastrar() {
-  if (
-    validacaoNome &&
-    validacaoEmail &&
-    validacaoTelefone &&
-    validacaoMensagem
-  ) {
-    alert("Formulário enviado com sucesso!");
-  }
+  formControl.className = "form-control success";
 }
